03aa90940c4de9851dba864be274c051d82b8cbe
yashvsyash <139322042+yashvsyash@users.noreply.github.com>
2023-07-13 12:06:13 +0530

    Add files via upload

---
+++ while for loop.py
@@ -0,0 +1,58 @@
+#while Loop
+
+"""x=20
+while x>=20 and x<=50:
+    print(x)
+    x+=2
+print(" JAI KEDAR")
+
+
+#while Loop
+
+x=int(input("Enter Number"))
+i=x
+while i<=x*10:
+    print(i)
+    i+=x
+print(" END")
+
+
+x=int(input("Enter Number"))
+t=1
+while t<=10:
+    print(x*t)
+    t+=1
+print(" END")
+
+
+#FACTORIAL of a given number{case1}
+
+x=int(input("Enter a Number"))
+f=x
+while x>=2:
+    x=(x-1)
+    f=f*x
+print(f)
+
+
+
+#FACTORIAL of a given number{case2}
+
+n=int(input("Enter a Number"))
+x=1
+fac=1
+while x<=n:
+    fac=fac*x
+    x=(x+1)
+print(fac)"""
+
+
+
+#FACTORIAL of a given number{case3}
+
+n=int(input("Enter a Number"))
+fact=1
+while n>0:
+    fact=fact*n
+    n-=1
+print(fact)
